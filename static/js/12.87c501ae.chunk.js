dd5e6ee96dc6105e5b71f2be0ae8b7aa2ec1ccd8
Marlair Bertrand <4173378-BertrandMarlair@users.noreply.gitlab.com>
2021-06-20 13:10:30 +0200

    Updates

---
+++ static/js/12.87c501ae.chunk.js
@@ -0,0 +1,1 @@
+(this.webpackJsonpnewclient=this.webpackJsonpnewclient||[]).push([[12],{269:function(n,e,t){"use strict";t.r(e);var c=t(0),i=t(2);e.default=function(n){return Object(i.jsx)(c.Fragment,{children:n.children})}}}]);
