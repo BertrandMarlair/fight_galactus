e8dc54f576b969c08dc37e3df1485c06d3bd12f9
Marlair Bertrand <4173378-BertrandMarlair@users.noreply.gitlab.com>
2021-06-25 22:38:19 +0200

    Updates

--- env.js
+++ env.js
@@ -1,12 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 const env = {
-    // domain: "playground.b-fine.be",
-    // protocolHTTP: "https",
-    // protocolWS: "wss",
-    // domain: "192.168.1.59:3001",
-    // protocolHTTP: "http",
-    // protocolWS: "ws",
     domain: "localhost:3001",
     protocolHTTP: "http",
     protocolWS: "ws",
+    started: true,
 };
